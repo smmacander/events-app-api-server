9fc0b7712fe85650ca2b4770cca4bfcef0951082
smmacander <smmacander@gmail.com>
2022-10-11 16:26:34 +0000

    Added a comment for the branching activity

--- src/app.config.js
+++ src/app.config.js
@@ -2,6 +2,7 @@
  * Place to store data specific to this instance
  * DO NOT store secrets here
  * DO NOT store state here - must be part of a stateless service
+ * This is a change for the branching activity.
  */
 class AppConfig {
     team = 'Sean Macander';
